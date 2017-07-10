13258db85ca7d79b867f781bae460fa83694885a
Adam Kapelner <kapelner@gmail.com>
2017-07-10 17:45:55 -0400

    better error message

--- demos/demo_quantile_yarf.R
+++ demos/demo_quantile_yarf.R
@@ -2,57 +2,102 @@ options(java.parameters = c("-Xmx4000m"))
 library(YARF)
 set_YARF_num_cores(7)
 
+quantile_loss = function(quantile, y, yhat){
+  error = 0
+  for (i in 1 : length(y)){
+    diff = y[i] - yhat[i];
+    if (diff > 0){
+      error = error + quantile * diff;
+    }
+    else {
+      error = error + (quantile - 1) * diff;			
+    }
+  }
+  error / length(y)
+}
+
 ##### the case of the exponential errors
 #### true mean is x + 1/gamma
 
-n = 500
-gamma = 7
-quantile = "0.90"
-X = data.frame(x1 = (sort(runif(n, 0, 1))))
-y = 0 + 1 * X[,1] + rexp(n, gamma)
-plot(X$x1, y, xlab = "x")
-abline(a = 1 / gamma, b = 1, col = "gray", lwd = 3)
-true_quantile = X$x1 + qexp(as.numeric(quantile), gamma)
-points(X$x1, true_quantile, type = "l", col = "red")
+ntrain = 500
+gamma = 5
+quantile = 0.01
+Nsim = 1000  
+num_trees = 500
+nodesize = 50
+ntest = 1000
 
-yarf_mod_vanilla = YARF(X, y, num_trees = 500)
-yarf_mod_vanilla
-points(X$x1, predict(yarf_mod_vanilla, X), type = "l", col = "darkgreen", lwd = 1)
-YARF_update_with_oob_results(yarf_mod_vanilla, oob_cost_calculation_script = 
-                               gsub("quantile_from_R", quantile,
-                                    read_file("js/quantile_cost_oob.js")))
+res = matrix(NA, nrow = Nsim, ncol = 5)
 
-yarf_mod = YARF(X, y, 
-              num_trees = 2000, 
-              nodesize = 100,
-              node_assign_script = read_file("js/null_assignment.js"),
-              cost_single_node_calc_script = gsub("quantile_from_R", quantile,
-                                                  read_file("js/quantile_cost.js"))
-            )
-YARF_update_with_oob_results(yarf_mod, oob_cost_calculation_script = 
-                               gsub("quantile_from_R", quantile,
-                                    read_file("js/quantile_cost_oob.js")))
-
-
-plot(X$x1, y, xlab = "x")
-points(X$x1, true_quantile, type = "l", col = "red")
-points(X$x1, predict(yarf_mod, X), type = "l", col = "blue", lwd = 3)
-
-library(quantregForest)
-qrf <- quantregForest(X, y, ntree = 2000, nthread = 7)
-conditionalQuantiles <- predict(qrf, X, what = as.numeric(quantile))
-points(X$x1, conditionalQuantiles, type = "l", col = "orange", lwd = 1)
-#find error for quantreg
-error = 0
-for (i in 1 : nrow(X)){
-  diff = true_quantile[i] - conditionalQuantiles[i];
-  if (diff > 0){
-    error = error + as.numeric(quantile) * diff;
-  }
-  else {
-    error = error + (as.numeric(quantile) - 1) * diff;			
-  }
+cef = function(x){
+  x^4
 }
-error / nrow(X)
-#we beat quantregForest!
 
+for (nsim in 1 : Nsim){
+  Xtrain = data.frame(x1 = (sort(runif(ntrain, 0, 1))))
+  
+  Xtest = data.frame(x1 = seq(0, 1, length.out = ntest))
+  ytrain = 0 + 2 * cef(Xtrain$x1) + rexp(nrow(Xtrain), gamma)
+  ytest_true = 2 * cef(Xtest$x1) + qexp(quantile, gamma) 
+  ytest = 2 * cef(Xtest$x1) + rexp(nrow(Xtest), gamma)
+  
+  
+  # plot(X$x1, y, xlab = "x")
+  # abline(a = 1 / gamma, b = 1, col = "gray", lwd = 3)
+  # true_quantile = X$x1 + qexp(as.numeric(quantile), gamma)
+  # points(X$x1, true_quantile, type = "l", col = "red")
+  # 
+  # yarf_mod_vanilla = YARF(X, y, num_trees = 500)
+  # yarf_mod_vanilla
+  # points(X$x1, predict(yarf_mod_vanilla, X), type = "l", col = "darkgreen", lwd = 1)
+  # YARF_update_with_oob_results(yarf_mod_vanilla, oob_cost_calculation_script = 
+  #                                gsub("quantile_from_R", quantile,
+  #                                     read_file("js/quantile_cost_oob.js")))
+  
+
+  
+  yarf_mod = YARF(Xtrain, ytrain, 
+                  num_trees = num_trees, 
+                  nodesize = nodesize,
+                  node_assign_script = read_file("js/null_assignment.js"),
+                  cost_single_node_calc_script = gsub("quantile_from_R", as.character(quantile),
+                                                      read_file("js/quantile_cost.js"))
+  )
+  YARF_update_with_oob_results(yarf_mod, oob_cost_calculation_script = 
+                                 gsub("quantile_from_R", as.character(quantile),
+                                      read_file("js/quantile_cost_oob.js")))
+  
+  quantile = as.numeric(quantile)
+  
+  
+  library(quantregForest)
+  qrf <- quantregForest(Xtrain, ytrain, ntree = num_trees, nodesize = nodesize, nthread = 7, importance = TRUE)
+  
+  #see who's better
+  yhat_yarf <- predict(yarf_mod, Xtest)
+  yhat_qrf <- predict(qrf, Xtest, what = quantile)
+  
+  #plot it for pretty pics
+  plot(Xtrain$x1, ytrain, xlab = "x")
+  points(Xtest$x1, ytest_true, type = "l", col = "red")
+  points(Xtest$x1, ytest, col = "yellow")
+  points(Xtest$x1, yhat_yarf, type = "l", col = "blue", lwd = 3)
+  points(Xtest$x1, yhat_qrf, type = "l", col = "brown", lwd = 2)
+  
+  #find error
+  ql_yarf = quantile_loss(quantile, ytest, yhat_yarf)
+  ql_qrf = quantile_loss(quantile, ytest, yhat_qrf)
+  ql_t_yarf = quantile_loss(quantile, ytest_true, yhat_yarf)
+  ql_t_qrf = quantile_loss(quantile, ytest_true, yhat_qrf) 
+  mse_yarf = sum((ytest_true - yhat_yarf)^2)
+  mse_qrf = sum((ytest_true - yhat_qrf)^2)
+  res[nsim, 1] = ql_yarf
+  res[nsim, 2] = ql_qrf
+  res[nsim, 3] = (ql_qrf - ql_yarf) / ql_qrf * 100
+  res[nsim, 4] = mse_yarf
+  res[nsim, 5] = mse_qrf
+  print(sort(res[1 : nsim, 3]))
+  print(mean(res[1 : nsim, 3]))
+  save(res, file = "quantile_res.RData")
+}
+res[1 : nsim, ]
