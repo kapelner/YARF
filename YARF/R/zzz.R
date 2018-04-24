828ea2627bd0f4baefd8b5b56266aeca8e247515
Adam Kapelner <kapelner@gmail.com>
2018-04-24 13:24:27 -0400

    fix issue with num cores default on MAC

--- YARF/R/zzz.R
+++ YARF/R/zzz.R
@@ -6,15 +6,24 @@
 
 .onAttach = function(libname, pkgname){
   num_gigs_ram_available = .jcall(.jnew("java/lang/Runtime"), "J", "maxMemory") / 1e9
+  
+  #as a default, assign all cores save 1 or tell the user it cannot be detected
+  num_cores = as.numeric(Sys.getenv('NUMBER_OF_PROCESSORS'))
+  num_cores_message = "Parallelization is by default and YARF uses all available CPU cores save one.\nYou can change this via \"set_YARF_num_cores()\".\n"
+  if (is.na(num_cores)){
+	  num_cores_message = "YARF was unable to detect the number of CPU cores.\nYou can specify the number of cores available via \"set_YARF_num_cores()\" if you want parallelization.\n"
+  } 
+  
   packageStartupMessage(
 	paste("Welcome to YARF v", VERSION, 
 			"! You have ", round(num_gigs_ram_available, 2), 
 			"GB memory available.\n\n", 
-			"If you run out of memory, restart R, and use e.g.\n'options(java.parameters = \"-Xmx5g\")' for 5GB of RAM before you call\n'library(YARF)'.\n\n",
-			"By default, YARF uses all available CPU cores save one.\nYou can change this via \"set_YARF_num_cores()\".\n",
+			"If you run out of memory, restart R, and use e.g.\n'options(java.parameters = \"-Xmx5g\")' for 5GB of RAM before you call\n'library(YARF)'.\n",
+			num_cores_message,
 			sep = "")
   )
-  #as a default, assign all cores save 1
-  num_cores = as.numeric(Sys.getenv('NUMBER_OF_PROCESSORS'))
-  set_YARF_num_cores(max(num_cores - 1, 1))
+  if (!is.na(num_cores)){
+	  set_YARF_num_cores(max(num_cores - 1, 1))
+  }
+  
 }
