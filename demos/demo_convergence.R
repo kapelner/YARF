c717c3bd3cb5fcfe6efec0aeeb5768b7a0ab22c5
Adam Kapelner <kapelner@gmail.com>
2020-08-02 02:23:25 -0400

    fix for random seed stuff

--- demos/demo_convergence.R
+++ demos/demo_convergence.R
@@ -5,6 +5,6 @@ library(MASS); data(Boston)
 X = Boston[, 1 : 13]; y = Boston[, 14]
 
 set_YARF_num_cores(7)
-yarf_mod = YARF(X, y, fit_until_convergence = TRUE, tolerance = 0.0001)
+yarf_mod = YARF(X, y, fit_until_convergence = TRUE, tolerance = 0.00001)
 YARF_convergence(yarf_mod)
 YARF_update_with_oob_results(yarf_mod)
