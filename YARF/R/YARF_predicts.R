3aac303996e2ea102dd46b9493945d4ac4c3d96c
Adam Kapelner <kapelner@gmail.com>
2025-04-18 10:16:12 -0400

    upgraded YARF for latest R version

--- YARF/R/YARF_predicts.R
+++ YARF/R/YARF_predicts.R
@@ -59,7 +59,7 @@ predict.YARF = function(object, new_data, ...){
 	
 	y_hats = .jcall(object$java_YARF, "[D", "Evaluate", .jarray(new_data, dispatch = TRUE), num_cores, simplify = TRUE)
 	if (object$pred_type == "classification"){ #convert back to the native factor representation
-		y_hats = factor(y_hats, labels = object$y_levels, levels = object$y_levels)
+		y_hats = factor(y_hats, labels = object$y_levels)
 	}
 	y_hats
 	
